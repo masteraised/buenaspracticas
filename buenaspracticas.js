655f3de66d2dde70993bdfce3694167c96bd88de
masteraised <137823193+masteraised@users.noreply.github.com>
2023-09-06 16:02:07 -0300

    Update buenaspracticas.js

--- buenaspracticas.js
+++ buenaspracticas.js
@@ -4,9 +4,8 @@ let userAge = 25;
 //Funcion para comprobar los requisitos
 function checkUserRequisites(){
     let tieneAuto = true;
-    if (userAge >= 18){
-        if(tieneAuto===true){
-            return true;
+    if (userAge >= 18 && tieneAuto===true){
+        return true;
         }else {
             return false;
         }
